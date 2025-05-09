a5fa9830a11d3a47e2a6e6a31d86c934f8fcb327
Ayush Sharma <165563915+iamayushsharmaa@users.noreply.github.com>
2025-05-10 01:48:28 +0530

    Initial

--- lib/models/chat_model.dart
+++ lib/models/chat_model.dart
@@ -1,12 +1,32 @@
-class ChatModel{
+class ChatModel {
   final String role;
-  final List<ChatPartModel> part;
+  final List<ChatPartModel> parts;
 
-  ChatModel({required this.role, required this.part});
+  ChatModel({required this.role, required this.parts});
+
+  factory ChatModel.fromJson(Map<String, dynamic> json) => ChatModel(
+    role: json['role'],
+    parts: List<ChatPartModel>.from(
+      json['parts'].map((x) => ChatModel.fromJson(x)),
+    ),
+  );
+
+  Map<String, dynamic> toJson() => {
+    'role': role,
+    'parts': List<dynamic>.from(parts.map((x) => x.toJson())),
+  };
 }
 
-class ChatPartModel{
+class ChatPartModel {
   final String text;
 
   ChatPartModel({required this.text});
+
+  factory ChatPartModel.fromJson(Map<String, dynamic> json){
+    return ChatPartModel(text: json['text']);
+  }
+
+  Map<String, dynamic> toJson() => {
+    'text': text,
+  };
 }
--- lib/repository/chat_repository.dart
+++ lib/repository/chat_repository.dart
@@ -1,7 +1,11 @@
 import 'package:geminiai/models/chat_model.dart';
-
+import 'package:dio/dio.dart';
 class SpaceRepo{
   static chatTextGenerationRepo(List<ChatModel> previousMessage) async{
+    Dio dio = Dio();
+    
+    final response = dio.post('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDqP2szA8RnC4-Ae1iZd_PPTQ8eSbu4c-c',
 
+    );
   }
 }
